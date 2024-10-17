10801d32b48758a7f92ba57c6d24a0add8057985
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2024-10-18 05:36:27 +0900

    cpp-httplib

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,6 +1,8 @@
 ﻿#define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
+#include <thread>
+#include "httplib.h"
 
 #define TVTEST_PLUGIN_CLASS_IMPLEMENT
 #include "TVTestPlugin.h"
@@ -9,71 +11,106 @@
 // プラグインクラス
 class CHttpRemocon : public TVTest::CTVTestPlugin
 {
-	bool m_fEnabled = false;
+    bool m_fEnabled = false;
+    httplib::Server m_server;
+    std::thread m_serverThread;
 
-	static LRESULT CALLBACK EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void* pClientData);
-	static CHttpRemocon* GetThis(HWND hwnd);
+    static LRESULT CALLBACK EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void* pClientData);
+    static CHttpRemocon* GetThis(HWND hwnd);
+    void StartHttpServer();
+    void StopHttpServer();
 
 public:
-	bool GetPluginInfo(TVTest::PluginInfo* pInfo) override;
-	bool Initialize() override;
-	bool Finalize() override;
+    bool GetPluginInfo(TVTest::PluginInfo* pInfo) override;
+    bool Initialize() override;
+    bool Finalize() override;
 };
 
 
 bool CHttpRemocon::GetPluginInfo(TVTest::PluginInfo* pInfo)
 {
-	// プラグインの情報を返す
-	pInfo->Type = TVTest::PLUGIN_TYPE_NORMAL;
-	pInfo->Flags = TVTest::PLUGIN_FLAG_NONE;
-	pInfo->pszPluginName = L"HttpRemocon";
-	return true;
+    // プラグインの情報を返す
+    pInfo->Type = TVTest::PLUGIN_TYPE_NORMAL;
+    pInfo->Flags = TVTest::PLUGIN_FLAG_NONE;
+    pInfo->pszPluginName = L"HttpRemocon";
+    return true;
 }
 
 
 bool CHttpRemocon::Initialize()
 {
-	// 初期化処理
+    // 初期化処理
 
-	// イベントコールバック関数を登録
-	m_pApp->SetEventCallback(EventCallback, this);
+    // イベントコールバック関数を登録
+    m_pApp->SetEventCallback(EventCallback, this);
 
-	return true;
+    return true;
 }
 
 
 bool CHttpRemocon::Finalize()
 {
-	// 終了処理
+    // 終了処理
+    if (m_fEnabled) {
+        StopHttpServer();
+    }
 
-	return true;
+    return true;
 }
 
+
+void CHttpRemocon::StartHttpServer()
+{
+    // サーバが実行されていない場合にスレッドを開始
+    m_serverThread = std::thread([this]() {
+        m_server.Get("/", [](const httplib::Request& req, httplib::Response& res) {
+            res.set_content("Hello from TVTest plugin!", "text/plain");
+            });
+        m_server.listen("0.0.0.0", 8080);
+        });
+}
+
+
+void CHttpRemocon::StopHttpServer()
+{
+    if (m_server.is_running()) {
+        m_server.stop();
+    }
+    if (m_serverThread.joinable()) {
+        m_serverThread.join();  // サーバスレッドの終了を待機
+    }
+}
 
 // イベントコールバック関数
 // 何かイベントが起きると呼ばれる
 LRESULT CALLBACK CHttpRemocon::EventCallback(UINT Event, LPARAM lParam1, LPARAM lParam2, void* pClientData)
 {
-	CHttpRemocon* pThis = static_cast<CHttpRemocon*>(pClientData);
+    CHttpRemocon* pThis = static_cast<CHttpRemocon*>(pClientData);
 
-	switch (Event) {
-	case TVTest::EVENT_PLUGINENABLE:
-		// プラグインの有効状態が変化した
-		pThis->m_fEnabled = lParam1 != 0;
-		return TRUE;
-	}
+    switch (Event) {
+    case TVTest::EVENT_PLUGINENABLE:
+        // プラグインの有効状態が変化した
+        pThis->m_fEnabled = lParam1 != 0;
+        if (pThis->m_fEnabled) {
+            pThis->StartHttpServer();
+        }
+        else {
+            pThis->StopHttpServer();
+        }
+        return TRUE;
+    }
 
-	return 0;
+    return 0;
 }
 
 
 CHttpRemocon* CHttpRemocon::GetThis(HWND hwnd)
 {
-	return reinterpret_cast<CHttpRemocon*>(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
+    return reinterpret_cast<CHttpRemocon*>(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
 }
 
 
 TVTest::CTVTestPlugin* CreatePluginClass()
 {
-	return new CHttpRemocon;
+    return new CHttpRemocon;
 }
