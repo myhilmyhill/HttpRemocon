e4db691154a9174eca3b16d6629705a7ea81ce99
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2024-10-18 17:17:02 +0900

    音量、チャンネル、スリープなど

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,7 +10,12 @@
 #define TVTEST_PLUGIN_CLASS_IMPLEMENT
 #include "TVTestPlugin.h"
 
-void SimulateDropFiles(HWND hwndTarget, const std::wstring& filePath);
+static const char* defaultHost = "0.0.0.0";
+static const int defaultPort = 8080;
+static const char delimiter = ',';
+static void PrintChannel(std::ostringstream& output, const WCHAR* szDriver, const TVTest::ChannelInfo& ch);
+static void SimulateDropFiles(HWND hwndTarget, const std::wstring& filePath);
+static std::string WideCharToUTF8(const WCHAR* pWideChar);
 
 // プラグインクラス
 class CHttpRemocon : public TVTest::CTVTestPlugin
@@ -23,6 +28,8 @@ class CHttpRemocon : public TVTest::CTVTestPlugin
     static CHttpRemocon* GetThis(HWND hwnd);
     void StartHttpServer();
     void StopHttpServer();
+    std::string GetTunerList();
+    void SetChannel(const std::string& body, httplib::Response& res);
 
 public:
     bool GetPluginInfo(TVTest::PluginInfo* pInfo) override;
@@ -75,12 +82,52 @@ void CHttpRemocon::StartHttpServer()
             res.status = 200;
             });
 
+        m_server.Post("/", [this](const httplib::Request& req, httplib::Response& res) {
+            if (req.body == "close") {
+                m_pApp->Close();
+                res.status = 200;
+            }
+            else if (req.body == "sleep") {
+                // スリープ処理を別スレッドで実行
+                std::thread([this]() {
+                    // Windowsをスリープさせる
+                    HANDLE hToken;
+                    TOKEN_PRIVILEGES tp;
+
+                    // プロセスのトークンを取得
+                    OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
+
+                    // SE_SHUTDOWN_NAME権限を取得
+                    LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tp.Privileges[0].Luid);
+                    tp.PrivilegeCount = 1;
+                    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+                    // 権限を調整
+                    AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL);
+
+                    // スリープを実行
+                    SetSystemPowerState(TRUE, FALSE);
+                    }).detach();  // スレッドをデタッチして即座に戻る
+
+                res.status = 200;
+            }
+            else {
+                res.status = 400;
+                res.set_content("Invalid operation value", "text/plain");
+            }
+            });
+
         m_server.Post("/play", [this](const httplib::Request& req, httplib::Response& res) {
             std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
             std::wstring filePath = converter.from_bytes(req.body);
 
             // /tvtpipe はすでにあるものとみなす
-            m_pApp->SetDriverName(L"BonDriver_Pipe.dll");
+            if (!m_pApp->SetDriverName(L"BonDriver_Pipe.dll")) {
+                res.status = 500;
+                res.set_content("Failed SetDriverName", "text/plain");
+                return;
+            }
+            // ServiceId が正常に 0 なのにエラー発生が返る。エラーチェックはしない
             m_pApp->SetChannel(0, 0);
 
             // ドラッグアンドドロップとしてファイルを開く
@@ -92,12 +139,178 @@ void CHttpRemocon::StartHttpServer()
             res.status = 200;
             });
 
-        m_server.set_error_handler([](const httplib::Request& req, httplib::Response& res) {
-            res.status = 404;
+        m_server.Get("/vol", [this](const httplib::Request& req, httplib::Response& res) {
+            int vol = m_pApp->GetVolume();
+            res.set_content(std::to_string(vol), "text/plain");
+            res.status = 200;
             });
 
-        m_server.listen("0.0.0.0", 8080);
+        m_server.Post("/vol", [this](const httplib::Request& req, httplib::Response& res) {
+            int volumeChange = std::stoi(req.body);
+            int currentVolume = m_pApp->GetVolume();
+
+            if (req.body[0] == '+' || req.body[0] == '-') {
+                // 相対値として設定
+                currentVolume += volumeChange;
+            }
+            else {
+                // 絶対値として設定
+                currentVolume = volumeChange;
+            }
+
+            // 音量が範囲内に収まるように制限
+            if (currentVolume < 0) currentVolume = 0;
+            if (currentVolume > 100) currentVolume = 100;
+
+            if (!m_pApp->SetVolume(currentVolume)) {
+                res.status = 500;
+                res.set_content("Failed SetVolume", "text/plain");
+                return;
+            }
+            res.set_content(std::to_string(currentVolume), "text/plain");
+
+            res.status = 200;
+            });
+
+        m_server.Get("/ch", [this](const httplib::Request& req, httplib::Response& res) {
+            res.set_content(GetTunerList(), "text/plain");
+            res.status = 200;
+            });
+
+        m_server.Post("/ch", [this](const httplib::Request& req, httplib::Response& res) {
+            SetChannel(req.body, res);
+            });
+
+        m_server.set_exception_handler([](const auto& req, auto& res, std::exception_ptr ep) {
+            auto fmt = "%s";
+            char buf[BUFSIZ];
+            try {
+                std::rethrow_exception(ep);
+            }
+            catch (std::exception& e) {
+                snprintf(buf, sizeof(buf), fmt, e.what());
+            }
+            catch (...) {
+                snprintf(buf, sizeof(buf), fmt, "Unknown Exception");
+            }
+            res.set_content(buf, "text/plain");
+            res.status = 500;
+            });
+
+        m_server.listen(defaultHost, defaultPort);
         });
+}
+
+
+void PrintChannel(std::ostringstream& output, const WCHAR* szDriver, const TVTest::ChannelInfo& ch) {
+    std::string sDriver = WideCharToUTF8(szDriver);
+    std::string sChannelName = WideCharToUTF8(ch.szChannelName);
+
+    output << sDriver << delimiter
+        << ch.Space << delimiter
+        << ch.Channel << delimiter
+        << ch.ServiceID << ": "
+        << sChannelName << "\n";
+}
+
+// チューナー/チャンネルのリストを取得する
+std::string CHttpRemocon::GetTunerList()
+{
+    std::ostringstream tunerList;
+    WCHAR szDriver[MAX_PATH];
+
+    {
+        TVTest::ChannelInfo ch;
+        m_pApp->GetCurrentChannelInfo(&ch);
+        m_pApp->GetDriverName(szDriver, _countof(szDriver));
+        PrintChannel(tunerList, szDriver, ch);
+        tunerList << "\n";
+    }
+    {
+        for (int i = 0; m_pApp->EnumDriver(i, szDriver, _countof(szDriver)) > 0; i++) {
+            TVTest::DriverTuningSpaceList spaces;
+            if (m_pApp->GetDriverTuningSpaceList(szDriver, &spaces)) {
+                for (DWORD j = 0; j < spaces.NumSpaces; j++) {
+                    const TVTest::DriverTuningSpaceInfo& chs = *spaces.SpaceList[j];
+                    for (DWORD k = 0; k < chs.NumChannels; k++) {
+                        const TVTest::ChannelInfo& ch = *chs.ChannelList[k];
+                        PrintChannel(tunerList, szDriver, ch);
+                    }
+                }
+
+                m_pApp->FreeDriverTuningSpaceList(&spaces);
+            }
+        }
+    }
+
+    return tunerList.str();
+}
+
+
+void CHttpRemocon::SetChannel(const std::string& body, httplib::Response& res) {
+    TVTest::ChannelSelectInfo info = {};
+    info.Size = sizeof(info);
+    info.Space = -1;
+    info.Channel = -1;
+    info.ServiceID = 0;
+
+    // req.body は "pszTuner,Channel,ServiceID" の形式
+    std::istringstream iss(body);
+    std::string tuner, space, channelStr, serviceIdStr;
+
+    if (std::getline(iss, tuner, delimiter) && std::getline(iss, space, delimiter) && std::getline(iss, channelStr, delimiter) && std::getline(iss, serviceIdStr)) {
+        if (!tuner.empty()) {
+            std::wstring wTuner = std::wstring(tuner.begin(), tuner.end());
+            info.pszTuner = wTuner.c_str();
+        }
+
+        if (!channelStr.empty()) {
+            try {
+                info.Channel = std::stoi(channelStr);
+            }
+            catch (const std::invalid_argument& e) {
+                res.status = 400;
+                res.set_content("Invalid Channel value", "text/plain");
+                return;
+            }
+        }
+
+        if (!space.empty()) {
+            try {
+                info.Space = std::stoi(space);
+            }
+            catch (const std::invalid_argument& e) {
+                res.status = 400;
+                res.set_content("Invalid Space value", "text/plain");
+                return;
+            }
+        }
+
+        if (!serviceIdStr.empty()) {
+            try {
+                info.ServiceID = std::stoi(serviceIdStr);
+            }
+            catch (const std::invalid_argument& e) {
+                res.status = 400;
+                res.set_content("Invalid ServiceID value", "text/plain");
+                return;
+            }
+        }
+
+        // チャンネル選択
+        if (!m_pApp->SelectChannel(&info)) {
+            res.status = 500;
+            res.set_content("Failed SelectChannel", "text/plain");
+            return;
+        }
+
+        res.status = 200;
+    }
+    else {
+        // パースエラー
+        res.status = 400;
+        res.set_content("Invalid request format", "text/plain");
+    }
 }
 
 
@@ -183,4 +396,16 @@ void SimulateDropFiles(HWND hwndTarget, const std::wstring& filePath)
             GlobalFree(hGlobal);
         }
     }
+}
+
+
+std::string WideCharToUTF8(const WCHAR* pWideChar)
+{
+    int len = WideCharToMultiByte(CP_UTF8, 0, pWideChar, -1, nullptr, 0, nullptr, nullptr);
+    if (len == 0) {
+        return "";
+    }
+    std::string utf8Str(len - 1, '\0'); // 終端文字分を引く
+    WideCharToMultiByte(CP_UTF8, 0, pWideChar, -1, &utf8Str[0], len, nullptr, nullptr);
+    return utf8Str;
 }
