765c88774472c6e958b0b3a1ad969fa1c6f6194c
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2024-10-20 09:00:52 +0900

    録画、キャプチャ

--- dllmain.cpp
+++ dllmain.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <codecvt>
 #include <shlobj_core.h>
+#include <filesystem>
+#include <chrono>
 #include "httplib.h"
 
 #define TVTEST_PLUGIN_CLASS_IMPLEMENT
@@ -23,9 +25,13 @@ static const UINT WM_TVTP_SEEK = WM_TVTP_APP + 60;
 static const UINT WM_TVTP_SEEK_ABSOLUTE = WM_TVTP_APP + 61;
 
 static void PrintChannel(std::ostringstream& output, const WCHAR* szDriver, const TVTest::ChannelInfo& ch);
+std::wstring convertUtf8ToWstring(const std::string& utf8);
+std::string convertWstringToUtf8(const std::wstring& wstr);
 static void SimulateDropFiles(HWND hwndTarget, const std::wstring& filePath);
 static std::string WideCharToUTF8(const WCHAR* pWideChar);
 static int ParseTimeToMilliseconds(const std::string& input);
+std::string findRecentBMPFile(const std::wstring& directory, const std::chrono::system_clock::time_point& lastSaveTime);
+std::vector<char> readFile(const std::string & filePath);
 
 // プラグインクラス
 class CHttpRemocon : public TVTest::CTVTestPlugin
@@ -112,8 +118,7 @@ void CHttpRemocon::StartHttpServer()
             });
 
         m_server.Post("/play", [this](const httplib::Request& req, httplib::Response& res) {
-            std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-            std::wstring filePath = converter.from_bytes(req.body);
+            std::wstring filePath = convertUtf8ToWstring(req.body);
 
             // /tvtpipe はすでにあるものとみなす
             if (!m_pApp->SetDriverName(L"BonDriver_Pipe.dll")) {
@@ -194,7 +199,6 @@ void CHttpRemocon::StartHttpServer()
             });
 
         m_server.Get("/play/speed", [this](const httplib::Request& req, httplib::Response& res) {
-            // TvtPlay を見てもあんまり柔軟なことはできなそう
             HWND hwnd = FindWindow(TEXT("TvtPlay Frame"), NULL);
             if (hwnd == NULL) {
                 res.status = 500;
@@ -216,8 +220,7 @@ void CHttpRemocon::StartHttpServer()
 
             if (!m_pApp->DoCommand(command.c_str())) {
                 res.status = 500;
-                std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-                std::string error_message = "Failed DoCommand: " + converter.to_bytes(command);
+                std::string error_message = "Failed DoCommand: " + convertWstringToUtf8(command);
                 res.set_content(error_message, "text/plain");
                 return;
             }
@@ -267,6 +270,130 @@ void CHttpRemocon::StartHttpServer()
             SetChannel(req.body, res);
             });
 
+        m_server.Get("/rec", [this](const httplib::Request& req, httplib::Response& res) {
+            TVTest::RecordStatusInfo status = {};
+            m_pApp->GetRecordStatus(&status);
+
+            res.status = 200;
+            switch (status.Status) {
+            case TVTest::RECORD_STATUS_NOTRECORDING:
+                res.set_content("Not recording", "text/plain");
+                return;
+
+            case TVTest::RECORD_STATUS_RECORDING:
+                res.set_content("Recording", "text/plain");
+                return;
+
+            case TVTest::RECORD_STATUS_PAUSED:
+                res.set_content("Paused", "text/plain");
+                return;
+
+            default:
+                res.status = 500;
+                res.set_content("Invalid status", "text/plain");
+                return;
+            }
+            });
+
+        m_server.Post("/rec", [this](const httplib::Request& req, httplib::Response& res) {
+            TVTest::RecordStatusInfo status = {};
+
+            if (req.body == "start") {
+                m_pApp->GetRecordStatus(&status);
+                if (status.Status == TVTest::RECORD_STATUS_RECORDING) {
+                    res.status = 400;
+                    res.set_content("Already start recording", "text/plain");
+                    return;
+                }
+
+                if (!m_pApp->DoCommand(L"TimeShiftRecording")) {
+                    res.status = 500;
+                    res.set_content("Failed DoCommand: TimeShiftRecording", "text/plain");
+                    return;
+                }
+
+                if (!m_pApp->DoCommand(L"RecordEvent")) {
+                    res.status = 500;
+                    res.set_content("Failed DoCommand: RecordEvent", "text/plain");
+                    return;
+                }
+
+                // 録画ファイル名を取得
+                WCHAR fileName[MAX_PATH] = {};
+                status.pszFileName = fileName;
+                status.MaxFileName = MAX_PATH;
+                m_pApp->GetRecordStatus(&status);
+
+                res.set_content(WideCharToUTF8(fileName), "text/plain");
+                res.status = 200;
+            }
+            else if (req.body == "stop") {
+
+                // 録画ファイル名を取得
+                WCHAR fileName[MAX_PATH] = {};
+                status.pszFileName = fileName;
+                status.MaxFileName = MAX_PATH;
+                m_pApp->GetRecordStatus(&status);
+
+                if (status.Status != TVTest::RECORD_STATUS_RECORDING) {
+                    res.status = 400;
+                    res.set_content("Not yet started recording", "text/plain");
+                    return;
+                }
+
+                if (!m_pApp->StopRecord()) {
+                    res.status = 500;
+                    res.set_content("Failed StopRecord", "text/plain");
+                    return;
+                }
+
+                res.set_content(WideCharToUTF8(fileName), "text/plain");
+                res.status = 200;
+            }
+            else {
+                res.status = 400;
+                res.set_content("Invalid operation value", "text/plain");
+            }
+            });
+
+        m_server.Post("/view/cap", [this](const httplib::Request& req, httplib::Response& res) {
+            // たぶん保存したキャプチャのファイル名がわからない。
+            // ファイルのタイムスタンプで頑張って探す。
+            // CaptureImageをした後にSaveImageは時間差ができるのでダメだった
+            auto saveStartTime = std::chrono::system_clock::now();
+
+            auto saved = m_pApp->SaveImage();
+            if (!saved) {
+                res.status = 500;
+                res.set_content("Failed SaveImage", "text/plain");
+                return;
+            }
+
+            // 相対パスの場合、あきらめる
+            WCHAR szFolder[MAX_PATH] = {};
+            if (m_pApp->GetSetting(L"CaptureFolder", szFolder, MAX_PATH) < 1) {
+                res.status = 500;
+                res.set_content("Failed GetSetting; CaptureFolder", "text/plain");
+            }
+
+            std::string bmpFilePath = findRecentBMPFile(std::wstring(szFolder), saveStartTime);
+            if (bmpFilePath.empty()) {
+                res.status = 500;
+                res.set_content("Invalid bmp file path", "text/plain");
+                return;
+            }
+
+            std::vector<char> bmpData = readFile(bmpFilePath);
+            if (bmpData.empty()) {
+                res.status = 500;
+                res.set_content("Failed to read bmp file", "text/plain");
+                return;
+            }
+
+            res.set_content(bmpData.data(), bmpData.size(), "image/bmp");
+            res.status = 200;
+            });
+
         m_server.Post("/view/panel", [this](const httplib::Request& req, httplib::Response& res) {
             // トグルしかできないので body は見ない
             if (!m_pApp->DoCommand(L"Panel")) {
@@ -279,9 +406,9 @@ void CHttpRemocon::StartHttpServer()
             });
 
         m_server.Post("/view/reset", [this](const httplib::Request& req, httplib::Response& res) {
-            if (!m_pApp->DoCommand(L"Reset")) {
+            if (!m_pApp->Reset(TVTest::RESET_VIEWER)) {
                 res.status = 500;
-                res.set_content("Failed DoCommand: Reset", "text/plain");
+                res.set_content("Failed Reset", "text/plain");
                 return;
             }
 
@@ -360,6 +487,7 @@ std::string CHttpRemocon::GetTunerList()
 void CHttpRemocon::SetChannel(const std::string& body, httplib::Response& res) {
     TVTest::ChannelSelectInfo info = {};
     info.Size = sizeof(info);
+    info.pszTuner = nullptr;
     info.Space = -1;
     info.Channel = -1;
     info.ServiceID = 0;
@@ -367,10 +495,11 @@ void CHttpRemocon::SetChannel(const std::string& body, httplib::Response& res) {
     // req.body は "pszTuner,Channel,ServiceID" の形式
     std::istringstream iss(body);
     std::string tuner, space, channelStr, serviceIdStr;
+    std::wstring wTuner;
 
     if (std::getline(iss, tuner, delimiter) && std::getline(iss, space, delimiter) && std::getline(iss, channelStr, delimiter) && std::getline(iss, serviceIdStr)) {
         if (!tuner.empty()) {
-            std::wstring wTuner = std::wstring(tuner.begin(), tuner.end());
+            wTuner = std::wstring(tuner.begin(), tuner.end());
             info.pszTuner = wTuner.c_str();
         }
 
@@ -468,6 +597,30 @@ TVTest::CTVTestPlugin* CreatePluginClass()
     return new CHttpRemocon;
 }
 
+// UTF-8 から UTF-16 への変換
+std::wstring convertUtf8ToWstring(const std::string& utf8) {
+    int wideCharSize = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, nullptr, 0);
+    if (wideCharSize == 0) {
+        throw std::runtime_error("Failed to convert UTF-8 to UTF-16");
+    }
+
+    std::wstring wstr(wideCharSize, L'\0');
+    MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, &wstr[0], wideCharSize);
+    return wstr;
+}
+
+// UTF-16 から UTF-8 への変換
+std::string convertWstringToUtf8(const std::wstring& wstr) {
+    int utf8Size = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    if (utf8Size == 0) {
+        throw std::runtime_error("Failed to convert UTF-16 to UTF-8");
+    }
+
+    std::string utf8(utf8Size, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &utf8[0], utf8Size, nullptr, nullptr);
+    return utf8;
+}
+
 // ファイルのドラッグアンドドロップをシミュレートする関数
 void SimulateDropFiles(HWND hwndTarget, const std::wstring& filePath)
 {
@@ -543,4 +696,36 @@ int ParseTimeToMilliseconds(const std::string& input) {
     }
 
     return (hours * 3600 + minutes * 60 + seconds) * 1000;
+}
+
+// 指定された時間範囲内のBMPファイルを探す関数
+std::string findRecentBMPFile(const std::wstring& directory, const std::chrono::system_clock::time_point& lastSaveTime) {
+    auto lastSaveFileTime = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
+        std::filesystem::file_time_type::clock::now() - (std::chrono::system_clock::now() - lastSaveTime));
+
+    for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".bmp") {
+            auto lastWriteTime = std::filesystem::last_write_time(entry);
+
+            if (lastWriteTime > lastSaveFileTime) {
+                return entry.path().string();
+            }
+        }
+    }
+    return ""; // 該当するファイルが見つからなければ空文字列を返す
+}
+
+
+std::vector<char> readFile(const std::string& filePath) {
+    std::ifstream file(filePath, std::ios::binary);
+    std::vector<char> buffer;
+    if (file) {
+        file.seekg(0, std::ios::end);
+        size_t fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        buffer.resize(fileSize);
+        file.read(buffer.data(), fileSize);
+    }
+    return buffer;
 }
